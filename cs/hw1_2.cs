5f52d0e9bec6d0aac068f762fcb6762ac67fb1b6
glebPo <potemskyi.gl@gmail.com>
2016-07-13 09:40:18 +0300

    Add files via upload

---
+++ cs/hw1_2.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Constr{
+
+	public	Constr(string str){
+
+		for(int i = 0; i < str.Length; i++){
+			Console.WriteLine(str[i] + ": dec" + (int)str[i] + " hex " + Convert.ToByte((int)str[i]).ToString("X"));
+		}
+	}
+
+}
+
+class Prog{
+	static void Main(){
+		Constr stringParse = new Constr("Hello world!");
+		Console.ReadKey();
+	}
+}
+
+
+//using System;
+
+//class Parse{
+//	static void Main(){
+//		string str = "Hello world!";
+//
+//		for(int i = 0; i < str.Length; i++){
+//			Console.WriteLine(str[i] + ": dec" + (int)str[i] + " hex " + Convert.ToByte((int)str[i]).ToString("X"));
+//		}
+//	}
+//}
